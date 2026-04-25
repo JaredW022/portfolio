4eef3c9ab7ec46fa4fec5e010877106c0a1bba0e
JaredW022 <jjw022@ucsd.edu>
2026-04-24 21:36:23 -0700

    test

--- global.js
+++ global.js
@@ -101,19 +101,6 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   });
 }
 
-export async function fetchGithubData(username) {
-  try {
-    const response = await fetch(`https://api.github.com/users/${username}/repos`);
-
-    if (!response.ok) {
-      throw new Error(`GitHub API error: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    return data;
-
-  } catch (error) {
-    console.error("Error fetching GitHub data:", error);
-    return [];
-  }
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
