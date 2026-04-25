9f16ae0d411f21501baa402c91c712c24350d5e6
JaredW022 <jjw022@ucsd.edu>
2026-04-24 19:48:40 -0700

    ch

--- projects/projects.js
+++ projects/projects.js
@@ -1,9 +1,7 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-async function loadProjects() {
-  const projects = await fetchJSON('../lib/projects.json');
-  const projectsContainer = document.querySelector('.projects');
-  renderProjects(projects, projectsContainer, 'h2');
-}
+const projects = await fetchJSON('../lib/projects.json');
 
-loadProjects();
+const projectsContainer = document.querySelector('.projects');
+
+renderProjects(projects, projectsContainer, 'h2');
