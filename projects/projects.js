418516b81bc32d5c418f4f7368963dcf50ba8c65
JaredW022 <jjw022@ucsd.edu>
2026-04-24 20:11:20 -0700

    test

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 
+const titleElement = document.querySelector('.projects-title');
+titleElement.textContent = 'Projects (${projects.length})';
+
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
