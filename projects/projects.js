ff36d7a11918c548d1541d079a3a68b75d64e2b3
JaredW022 <jjw022@ucsd.edu>
2026-05-01 17:10:18 -0700

    test

--- projects/projects.js
+++ projects/projects.js
@@ -38,9 +38,12 @@ for (let d of data) {
 
 let arcs = arcData.map((d) => arcGenerator(d));
 
+let colors = ['gold', 'purple'];
+
 arcs.forEach((arc, idx) => {
   d3.select('#projects-pie-plot')
   .append('path')
   .attr('d', arc)
   .attr('fill', colors[idx]);
 });
+
